455e9a6598e2b1a1f6c51b6afbc06274ad05782d
ritte <ritterhome15@gmail.com>
2024-01-18 13:22:51 -0500

    Made simulation window stay open longer

--- simulate.py
+++ simulate.py
@@ -1,4 +1,14 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+#disables sidebars
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+
+for i in range(1000):
+	p.stepSimulation()
+	time.sleep(1/60)
+	print(i)
+
 p.disconnect()
