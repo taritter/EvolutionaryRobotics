da62cf77e51cdfcfa056dcbb1bd1e585824c402a
ritte <ritterhome15@gmail.com>
2024-02-07 20:15:36 -0500

    updated joint position

--- generate.py
+++ generate.py
@@ -1,9 +1,54 @@
 import pyrosim.pyrosim as pyrosim
 
-# tell pyrosim the name of the file where information about the world you're about to create should be stored
-pyrosim.Start_SDF("box.sdf")
 
-# stores a box with initial position x=0, y=0, z=0.5, and length, width and height all equal to 1 meter, in box.sdf
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+def Create_World():
+    # tell pyrosim the name of the file where information about the world
+    # #you're about to create should be stored
+    pyrosim.Start_SDF("world.sdf")
 
-pyrosim.End()
+    # stores a box with initial position x=0, y=0, z=0.5, and length, width and height all equal to 1 meter, in box.sdf
+    x = 0
+    y = 0
+    z = 0.5
+
+    length = 1
+    width = 1
+    height = 1
+
+    length = 1
+    width = 1
+    height = 1
+    z = 0.5
+    #pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+
+
+    #pyrosim.Send_Cube(name="Box2", pos=[x,0.5,1.5] , size=[length,width,height])
+
+    pyrosim.End()
+
+def Create_Robot():
+    #makes the body
+    pyrosim.Start_URDF("body.urdf")
+
+    # parent
+    pyrosim.Send_Cube(name="Torso", pos=[1, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[1.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 1])
+    # child
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+
+
+
+
+    pyrosim.End()
+
+def main():
+    Create_World()
+    Create_Robot()
+
+
+if __name__ == "__main__":
+    main()
+
+
--- simulate.py
+++ simulate.py
@@ -1,12 +1,24 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 #disables sidebars
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-p.loadSDF("box.sdf")
+# add gravity
+p.setGravity(0,0,-9.8)
+
+# add floor
+planeId = p.loadURDF("plane.urdf")
+# blue robot body
+robotId = p.loadURDF("body.urdf")
+
+
+p.loadSDF("world.sdf")
 
 for i in range(1000):
 	p.stepSimulation()
