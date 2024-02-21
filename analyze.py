69a399f4091062f3e292321e9dff5b6640d02e65
ritte <ritterhome15@gmail.com>
2024-02-20 22:51:27 -0500

    moved away from screen

--- analyze.py
+++ analyze.py
@@ -1,11 +1,16 @@
 import numpy
 import matplotlib.pyplot
 
-back = numpy.load("data/backLeg.npy")
-front = numpy.load("data/frontLeg.npy")
+#back = numpy.load("data/backLeg.npy")
+#front = numpy.load("data/frontLeg.npy")
+angle_back = numpy.load("data/targetAngleBack.npy")
+angle_front = numpy.load("data/targetAngleFront.npy")
 
-matplotlib.pyplot.plot(back, label='Back Leg', linewidth=2)
-matplotlib.pyplot.plot(front, label='Front Leg', linewidth=2)
+
+#matplotlib.pyplot.plot(back, label='Back Leg', linewidth=2)
+#matplotlib.pyplot.plot(front, label='Front Leg', linewidth=2)
+matplotlib.pyplot.plot(angle_back, label='Back Angle', linewidth=6)
+matplotlib.pyplot.plot(angle_front, label='Front Angle', linewidth=2)
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
 
--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as numpy
+import math
+import random
 
 ITER = 1000
 physicsClient = p.connect(p.GUI)
@@ -13,12 +15,19 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
 # add gravity
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, -9.8)
 
 # add floor
 planeId = p.loadURDF("plane.urdf")
 # blue robot body
 robotId = p.loadURDF("body.urdf")
+
+amplitudeBack = numpy.pi/4
+amplitudeFront = numpy.pi/10
+frequencyBack = 6
+frequencyFront = 2
+phaseOffsetFront = numpy.pi/4
+phaseOffsetBack = numpy.pi/20
 
 
 p.loadSDF("world.sdf")
@@ -27,12 +36,18 @@ pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = numpy.zeros(ITER)
 frontLegSensorValues = numpy.zeros(ITER)
 
+targetAngleBack = numpy.sin(numpy.linspace(0, (2*numpy.pi), ITER))*(numpy.pi/4)
+targetAngleFront = numpy.sin(numpy.linspace(0, (2*numpy.pi), ITER))*(numpy.pi/4)
 
 
 for i in range(ITER):
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+	targetAngleBack = amplitudeBack * numpy.sin(numpy.linspace(0, (2*numpy.pi), ITER) * frequencyBack + phaseOffsetBack)
+	targetAngleFront = amplitudeFront * numpy.sin(numpy.linspace(0, (2*numpy.pi), ITER) * frequencyFront + phaseOffsetFront)
+
 
 	pyrosim.Set_Motor_For_Joint(
 		# what robot the motor is to be attached to
@@ -42,15 +57,31 @@ for i in range(ITER):
 		# how the motor will attempt to control the motion of the joint
 		controlMode=p.POSITION_CONTROL,
 		# the desired angle between the two links connected by the joint
-		targetPosition=0.0,
+		targetPosition=targetAngleBack[i],
 		# cap the total torque ever used by a motor
-		maxForce=500)
+		maxForce=200)
 
-	time.sleep(1/60)
+	pyrosim.Set_Motor_For_Joint(
+		# what robot the motor is to be attached to
+		bodyIndex=robotId,
+		# tells the simulator what joint the motor should be attached to
+		jointName=b'Torso_FrontLeg',
+		# how the motor will attempt to control the motion of the joint
+		controlMode=p.POSITION_CONTROL,
+		# the desired angle between the two links connected by the joint
+		targetPosition=targetAngleFront[i],
+		# cap the total torque ever used by a motor
+		maxForce=200)
+
+	time.sleep(1/100)
 	# print(i)
 print(backLegSensorValues)
 numpy.save("data/backLeg.npy", backLegSensorValues, allow_pickle=True, fix_imports=True)
 numpy.save("data/frontLeg.npy", frontLegSensorValues, allow_pickle=True, fix_imports=True)
+numpy.save("data/targetAngleBack.npy", targetAngleBack, allow_pickle=True, fix_imports=True)
+numpy.save("data/targetAngleFront.npy", targetAngleFront, allow_pickle=True, fix_imports=True)
+
+
 
 
 p.disconnect()
